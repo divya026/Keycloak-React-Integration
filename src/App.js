db709938f85dd841de2ac554d00bfb8e050013a3
Divya <divya.ttl@tatamotors.com>
2024-07-15 16:08:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+function App({ keycloak }) {
+  const logout = () => {
+    keycloak.logout({ redirectUri: window.location.origin });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My React App</h1>
+        {keycloak?.token ? (
+          <>
+            <p>Hello, {keycloak?.tokenParsed?.preferred_username}!</p>
+            <button onClick={logout}>Logout</button>
+            {/* <p>Token: {keycloak.token}</p> */}
+          </>
+        ) : (
+          <p>Not authenticated</p>
+        )}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import keycloak from "./keycloak";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+keycloak
+  .init({ onLoad: "login-required" })
+  .then((authenticated) => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App keycloak={keycloak} />
+      </React.StrictMode>,
+      document.getElementById("root")
+    );
+  })
+  .catch((error) => {
+    console.error("Keycloak initialization failed:", error);
+  });
